285cf01710169395345e4b03ce8d2f877b66b42d
Koko <kokicha.gg@gmail.com>
2022-07-11 11:02:56 +0300

    Initial Commit

---
+++ KokoGymFinaleProject/src/main/java/com/example/kokogymfinaleproject/KokoGymFinaleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kokogymfinaleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokoGymFinaleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokoGymFinaleProjectApplication.class, args);
+    }
+
+}
---
+++ KokoGymFinaleProject/src/test/java/com/example/kokogymfinaleproject/KokoGymFinaleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kokogymfinaleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokoGymFinaleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
